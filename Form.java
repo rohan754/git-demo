f3e7f4772faf7dcba561a01c79be0fa0f21d4fa4
Rohan <rohansonawane569@gmail.com>
2022-05-15 16:53:02 +0530

    Registration Added

---
+++ Form.java
@@ -0,0 +1,11 @@
+package excptionhandling;
+
+public class Form {
+
+	public static void main(String[] args) {
+		
+		System.out.println("We comes into the Form Page...");
+
+	}
+
+}
---
+++ Register.java
@@ -0,0 +1,11 @@
+package excptionhandling;
+
+public class Register {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Registration Page Created...");
+
+	}
+
+}
